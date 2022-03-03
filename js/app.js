71eda0f9b648476460d639d8e442155cee78c534
Khalidprithy <khalidprithy@gmail.com>
2022-03-03 19:52:02 +0600

    meal details added

--- js/app.js
+++ js/app.js
@@ -12,15 +12,16 @@ const searchMeals = () => {
             .then(res => res.json())
             .then(data => displayMeal(data.meals))
     }
-
 }
 
 const displayMeal = meals => {
 
     const resultDiv = document.getElementById('result-div');
+    const detailsDiv = document.getElementById('detail-div');
     const error1 = document.getElementById('error-1');
     const error2 = document.getElementById('error-2');
     error1.classList.add('hidden');
+    detailsDiv.textContent = '';
     resultDiv.textContent = '';
     if (meals === null) {
         error2.classList.remove('hidden');
@@ -41,7 +42,7 @@ const displayMeal = meals => {
                             <div>
                                 <h3 class="text-xl text-gray-700">${meal.strMeal}</h3>
                                 <p class="mt-1 text-xs text-gray-600 p-2">${meal.strInstructions.slice(0, 100)}</p>
-                                <button
+                                <button onclick="loadDetails(${meal.idMeal})"
                                     class="bg-teal-600 hover:bg-teal-800 text-slate-200 rounded-md py-0.5 px-2 text-sm">Read More</button>
                             </div>
                         </div>
@@ -53,6 +54,45 @@ const displayMeal = meals => {
 
         })
     }
+}
 
+const loadDetails = (mealId) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+}
+
+
+const displayMealDetails = meals => {
+    console.log(meals);
+    const detailsDiv = document.getElementById('detail-div');
+    const div = document.createElement('div');
+    detailsDiv.textContent = '';
+    div.innerHTML = `
+        <div class="group relative drop-shadow-lg border-2 p-2 rounded-md mx-auto">
+                <div
+                    class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-40 lg:w-72 lg:aspect-none">
+                    <img src="${meals.strMealThumb}" alt="Meal image"
+                        class="w-full h-64 object-center object-cover lg:w-full lg:h-64">
+                </div>
+                <div class="mt-4 flex justify-center">
+                    <div>
+                        <h3 class="text-xl text-gray-700">${meals.strMeal}</h3>
+                        <h4>${meals.strArea} </h4>
+                        <h4>${meals.strCategory} </h4>
+                        
+                        <p class="mt-1 text-xs text-gray-600 p-2">${meals.strInstructions.slice(0, 1000)}</p>
+
+                        <button onclick="mealDetails()"
+                            class="bg-red-600 hover:bg-red-800 text-slate-200 rounded-md py-0.5 px-2 text-sm">YouTube</button>
+                    </div>
+                </div>
+            </div>
+        `;
+    detailsDiv.appendChild(div);
+}
+
+const loadYoutube = () => {
 
 }
